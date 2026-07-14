6ccac50c3ec80cc2acb659c54f7dcb285630e3ba
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-07-14 13:12:32 +0530

    Time: 23 ms (6.17%), Space: 19.5 MB (94.03%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def strfilter(self,s):
+        n=""
+        for i in range(0,len(s)):
+            if "A"<=s[i]<="Z":
+                n+=chr(ord(s[i])+32)
+            elif ("a"<=s[i]<="z")or("0"<=s[i]<="9"):
+                n+=s[i]
+        return n
+    def isPalindrome(self, s: str) -> bool:
+        s=self.strfilter(s)
+        i,j=0,len(s)-1
+        while i<j:
+            if s[i]!=s[j]:
+                return False
+            i+=1
+            j-=1
+        return True
+
