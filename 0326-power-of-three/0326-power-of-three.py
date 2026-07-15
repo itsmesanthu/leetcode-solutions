d9047f1e5202143faa0f8cece3d8cb1a75ebf7d7
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-07-15 12:36:56 +0530

    Time: 4 ms (84.77%), Space: 19.4 MB (22.91%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -1,8 +1,10 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n<=0:
-            return False
-        while n%3==0:
-            n//=3
-        return n==1
+        # method one to solve this time complesity is o(log3 n)
+        # if n<=0:
+        #     return False
+        # while n%3==0:
+        #     n//=3
+        # return n==1
+        return n>0 and (3**19)%n==0
         
