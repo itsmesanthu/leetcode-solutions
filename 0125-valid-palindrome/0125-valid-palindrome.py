6c09f13918af912cbd87fd5b1906127d320ecf1f
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-07-23 11:13:28 +0530

    Time: 18 ms (6.24%), Space: 19.6 MB (76.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(0,len(s)):
             if "A"<=s[i]<="Z":
                 n+=chr(ord(s[i])+32)
-            elif ("a"<=s[i]<="z")or("0"<=s[i]<="9"):
+            if "a"<=s[i]<="z"or"0"<=s[i]<="9":
                 n+=s[i]
         return n
     def isPalindrome(self, s: str) -> bool:
